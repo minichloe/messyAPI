29e76fcc2942ba1f330007ed93e2f12075a1df89
Chloe Chong <chloechong.us@gmail.com>
2018-10-31 17:12:18 -0400

    Added new object for compiled results

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const axios = require('axios');
 
 // General function for http GET request
-const getData = async url => {
-  try {
-    const { data } = await axios.get(url);
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
-};
+const getData = url => axios.get(url);
+
+// Function to remove unwanted data from strings
+const convertData = str => {};
+
+// Function to get budget and change to number format
+const getBudget = async url => {};
 
 const sortAndPrintData = async () => {
   try {
     // Get array of films
-    const { results } = await getData(`http://oscars.yipitdata.com`);
+    const { data } = await getData(`http://oscars.yipitdata.com`);
+    const results = data.results;
 
     // Initialize array for detail URLs
     const detailURLs = [];
@@ -22,8 +22,15 @@ const sortAndPrintData = async () => {
     const newResults = [];
 
     results.forEach(x => {
-      const film = x.films.filter(film => film.Winner);
-      newResults.push(film);
+      // Initialize empty object to save wanted data from films
+      const oscarWinningFilm = {};
+      const film = x.films.filter(film => film.Winner)[0];
+      // Set film year, title, and budget
+      oscarWinningFilm.Year = x.year;
+      oscarWinningFilm.Title = film.Film;
+      newResults.push(oscarWinningFilm);
+
+      //
     });
 
     console.log(newResults);
