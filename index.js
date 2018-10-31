c12a3f061353517e29c6c45566927ad5ebc6751c
Chloe Chong <chloechong.us@gmail.com>
2018-10-31 16:36:44 -0400

    Sort data by winning film

--- index.js
+++ index.js
@@ -2,13 +2,34 @@ const axios = require('axios');
 
 // General function for http GET request
 const getData = async url => {
-  const { data } = await axios.get(url);
-  return data;
+  try {
+    const { data } = await axios.get(url);
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const sortAndPrintData = async () => {
-  const { results } = await getData(`http://oscars.yipitdata.com`);
-  console.log(results);
+  try {
+    // Get array of films
+    const { results } = await getData(`http://oscars.yipitdata.com`);
+
+    // Initialize array for detail URLs
+    const detailURLs = [];
+
+    // Initialize array for compiled results
+    const newResults = [];
+
+    results.forEach(x => {
+      const film = x.films.filter(film => film.Winner);
+      newResults.push(film);
+    });
+
+    console.log(newResults);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 sortAndPrintData();
